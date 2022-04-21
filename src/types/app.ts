70e4b96391804f73dbab0d2bc2977607bb2d1697
Andy Williams <andy@nonissue.org>
2022-04-21 06:38:04 -0600

    bump

--- src/types/app.ts
+++ src/types/app.ts
@@ -1,19 +1,4 @@
 import { Late, TagsOnLates } from '@prisma/client';
-import type { NextComponentType, NextPageContext } from 'next';
-import type { AppProps } from 'next/app';
-
-// eslint-disable-next-line @typescript-eslint/no-explicit-any
-export type PageWithLayout<P> = NextComponentType<NextPageContext, any, P> & {
-  getLayout?: (
-    page: JSX.Element,
-    layoutProps: Record<string, unknown>
-  ) => JSX.Element;
-};
-
-export type AppPropsWithLayout<P = Record<string, unknown>> = AppProps<P> & {
-  // eslint-disable-next-line @typescript-eslint/naming-convention
-  Component: PageWithLayout<P> & { theme: string };
-};
 
 type User = {
   id: number;
@@ -24,22 +9,6 @@ type User = {
   lates: Late[];
   tags: Tag[];
 };
-
-// type Late = {
-//   id: number;
-//   title?: string;
-//   url: string;
-//   content?: string;
-//   published: boolean;
-
-//   tags: Tag[];
-
-//   owner: User;
-//   ownerId?: number;
-
-//   createdAt: Date;
-//   updatedAt: Date;
-// }
 
 type Tag = {
   id: number;
