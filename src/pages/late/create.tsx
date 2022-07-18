8728bb1c03bbcd554d45bee8197772a06192b4fd
Andy Williams <andy@nonissue.org>
2022-07-17 22:25:10 -0600

    form styling changes (looks great)

--- src/pages/late/create.tsx
+++ src/pages/late/create.tsx
@@ -1,10 +1,10 @@
 import { useForm } from "react-hook-form";
-import { Prisma, Late } from "@prisma/client";
+import { Late } from "@prisma/client";
 import { ReactElement } from "react";
 
 import React from "react";
 
-type FormValues = Omit<Prisma.LateCreateInput, "owner" | "category" | "tags">;
+// type FormValues = Omit<Prisma.LateCreateInput, "owner" | "category" | "tags">;
 
 // const test = Prisma.validator<Prisma.LateArgs>()({
 //   select: { tags: false, category: false, owner: false },
@@ -12,7 +12,7 @@ type FormValues = Omit<Prisma.LateCreateInput, "owner" | "category" | "tags">;
 
 type lateFormValues = Late;
 
-type test = Prisma.LateCreateArgs;
+// type test = Prisma.LateCreateArgs;
 
 type FormGroupProps = {
   labelText: string;
@@ -22,12 +22,12 @@ type FormGroupProps = {
 
 const FormGroup = ({ labelText, children, groupStyle }: FormGroupProps) => {
   const StyledFormInput = React.cloneElement(children, {
-    className: `${children.props.className} mt-1 block w-full rounded-md border-slate-300 shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-20 `,
+    className: `${children.props.className} mt-1 block w-full rounded-md border-indigo-200 shadow-sm focus:border-indigo-400 focus:ring focus:ring-indigo-200 focus:ring-opacity-20 `,
   });
 
   return (
     <label className={`block ${groupStyle}`}>
-      <span className="text-slate-500 text-sm font-[400]">{labelText}</span>
+      <span className="text-sm font-[400] text-slate-500">{labelText}</span>
 
       {StyledFormInput}
     </label>
@@ -36,13 +36,13 @@ const FormGroup = ({ labelText, children, groupStyle }: FormGroupProps) => {
 
 const FormCheckbox = ({ labelText, children }: FormGroupProps) => {
   const StyledFormInput = React.cloneElement(children, {
-    className: `${children.props.className}  rounded-md scale-[80%] border-slate-300 shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-20`,
+    className: `${children.props.className}  rounded scale-[80%] border-indigo-300 shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-20`,
   });
 
   return (
-    <label className="flex items-center px-1 justify-center">
+    <label className="flex items-center justify-center px-1">
       {StyledFormInput}
-      <span className="text-slate-500 text-sm font-[400] p-1 ">
+      <span className="p-1 text-sm font-[400] text-slate-500 ">
         {labelText}
       </span>
     </label>
@@ -58,13 +58,14 @@ export default function LateCreate() {
   } = useForm<lateFormValues>();
 
   const onSubmit = handleSubmit((data) => console.log(data));
+  console.log(errors);
 
   return (
-    <div className="max-w-xl p-4 mx-auto mt-24 grid py-4 divide-y-2 divide-slate-200 border-slate-200 border-b-2 text-slate-600">
-      <h1 className="font-sans font-semibold  tracking-tight text-xl text-indigo-800 py-4">
+    <div className="mx-auto grid max-w-lg space-y-4 border-t-4 border-slate-300 p-4 font-sans text-slate-600 sm:mx-auto sm:px-0 md:mt-[10vh] md:max-w-2xl md:space-y-4 md:rounded-2xl md:border-2 md:border-indigo-200 md:p-6 md:shadow-md md:shadow-indigo-900/[6%] ">
+      <h1 className="text-xl font-semibold tracking-tight text-indigo-800">
         Create Late
       </h1>
-      <form onSubmit={onSubmit} className="grid md:grid-cols-2 gap-4 pt-4">
+      <form onSubmit={onSubmit} className="grid gap-4 py-2 md:grid-cols-2">
         <FormGroup labelText="URL">
           <input {...register("url")} type="url" />
         </FormGroup>
@@ -81,8 +82,8 @@ export default function LateCreate() {
         <FormGroup labelText="Notes" groupStyle="md:col-span-2">
           <textarea {...register("notes")} />
         </FormGroup>
-        <div className="grid grid-cols-1 md:col-span-1 gap-4 content-center">
-          <div className="grid grid-cols-3 ">
+        <div className="grid grid-cols-1 content-center gap-4 md:col-span-1">
+          <div className="grid grid-cols-3 divide-x  divide-indigo-100 py-2 ">
             <FormCheckbox labelText="Favorite">
               <input {...register("favorite")} type="checkbox" />
             </FormCheckbox>
@@ -97,7 +98,7 @@ export default function LateCreate() {
         <button
           type="button"
           onClick={() => onSubmit()}
-          className="m-1 col-span-1 md:col-start-2 text-sm font-semibold text-indigo-500 bg-indigo-50/0 border-2 block form-input rounded-md border-indigo-300  shadow-sm hover:border-indigo-500 hover:ring hover:ring-indigo-200 hover:text-indigo-700 hover:ring-opacity-20 hover:bg-white duration-200"
+          className="form-input col-span-1 m-1 block rounded-md border-2 border-indigo-300 bg-indigo-50/0 text-sm font-semibold text-indigo-500 shadow-sm  duration-200 hover:border-indigo-500 hover:bg-white hover:text-indigo-700 hover:ring hover:ring-indigo-200 hover:ring-opacity-20 md:col-start-2"
         >
           Create Late
         </button>
